b3026007f475b8ef36c7392ac75e43409572297a
Veena Kuruba <javavk2022@gmail.com>
2025-07-02 12:19:42 -0700

    Python AI

---
+++ main.py
@@ -0,0 +1,13 @@
+#learing with Github copilot
+First="Learn Python with AI pairing"
+Second="Python_language_is_easy_to_learn"
+# get the length of the First string
+print(len(First))
+# Remove spaces and find the length
+no_spaces = First.replace(" ", "")
+print(len(no_spaces))
+# get the length of the Second string
+print(len(Second))
+# Remove underscores and find the length of the second string
+no_unsderscores=Second.replace("_","")
+print(len(no_unsderscores))
