6ebbfff6c7bea1d67e92bc9280d9c803ca95ae7a
Veena Kuruba <javavk2022@gmail.com>
2025-07-03 12:25:25 -0700

    adding emaployee details

--- func.py
+++ func.py
@@ -2,7 +2,8 @@
 # pylint: disable=too-few-public-methods
 class Emp:
     """A class representing an employee with name, age, salary, and job title."""
-
+    d_count=0
+    t_count=0
     #function employee
     def __init__(self, name, age, salary, job):
         self.name = name
@@ -11,17 +12,34 @@ class Emp:
         self.job = job
 
 
-emp = Emp('veena', 25, 25000, 'QA')
-emp1 = Emp('sita', 30, 30000, 'Dev')
+emp = Emp('seena', 25, 25000, 'QA')
+emp1 = Emp('rita', 30, 30000, 'Dev')
+emp3 = Emp('deena', 25, 25000, 'QA')
+emp4 = Emp('meeta', 30, 30000, 'Dev')
 print(emp.name, emp.age, emp.salary, emp.job)
 print(emp1.name, emp1.age, emp1.salary, emp1.job)
 
 
-#function to print employee details
-def employee_details(employee):
-    """Prints the details of an employee."""
-    print(f"Name:{employee.name},Age:{employee.age},Salary:{employee.salary},Job:{employee.job}")
 
+#function to add teams
+def add_dev_teams(dev_team):
+    """Adding dev team members"""
+    if dev_team.job=='Dev':
+        Emp.d_count=Emp.d_count+1
+        print(f"Developer count: {Emp.d_count}")
+        print(f"Develpers details:{dev_team.name}, {dev_team.age}, {dev_team.salary}, {dev_team.job}") 
 
-employee_details(emp)
-employee_details(emp1)
+    else:
+        Emp.t_count=Emp.t_count+1
+        print(f"total testers{Emp.t_count}")
+        print(f"Testers details:{dev_team.name}, {dev_team.age}, {dev_team.salary}, {dev_team.job}") 
+
+    return Emp.d_count, Emp.t_count
+        
+devt=add_dev_teams(emp1)
+testt=add_dev_teams(emp)
+devt1=add_dev_teams(emp4)
+testt2=add_dev_teams(emp3)
+
+#employee_details(emp1)
+print(f"total developers:{Emp.d_count},total esters:{Emp.t_count}")
