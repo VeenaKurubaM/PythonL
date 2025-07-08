a5c8c0fca496e961a97ee1f68a611a3435112847
Veena Kuruba <javavk2022@gmail.com>
2025-07-08 16:33:24 -0700

    addeded constructor

--- caculator.py
+++ caculator.py
@@ -2,28 +2,32 @@
 class Calculator:
     """Calculator"""
     #ignore warnings
-    
+ 
     def __init__(self,operator):
         """Constructor for Calculator class"""
         self.operator = operator
+        
         #operator=input("Enter your opetaor(+,*,-,/): ")
         num1=float(input("Enter the first number: "))
         num2=float(input("Enter the second number: "))
-        result= 0
+       
         if operator =="+":
-            print(num1+num2)
+            self.result = num1 + num2
         elif operator== "-":
-            print(num1-num2)
+            self.result=num1/num2
         elif operator=="*":
-            print(num1*num2)
+            self.result=num1*num2
         elif operator=="/":
-            print(num1/num2)
+            self.result=num1/num2
             
         else:
             print("Invalid operator")
+
           
        
 calc =Calculator(input("Enter your operator (+, *, -, /): "))
+print(f"Result:{calc.result}")
+
 
                   
    
