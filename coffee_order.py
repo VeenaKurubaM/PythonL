b45b37f0b7bbb5c5e1341deb2513cdd489801bd1
VK <42256210+VeenaKurubaM@users.noreply.github.com>
2025-07-09 14:18:46 -0700

    Update coffee_order.py

--- coffee_order.py
+++ coffee_order.py
@@ -5,7 +5,7 @@ cofee=input("Enter your favourite coffee: Capuccino Espresso Latte ")
 payment_option=input("Enter your payment option: Cash Credit Card Debit Card: ")
 amount=5.50
 
-# Format the out put with f string
+# Format the output with f string
 print(f"Welcome {first_name} to our Restaurant!")
 print(f"You have ordered {cofee} and payment option is: {payment_option} is this right?")
 if payment_option =="1":
