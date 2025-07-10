3b02c2529b6c3808454ff1b196163ffb6d06dd3d
Veena Kuruba <javavk2022@gmail.com>
2025-07-09 21:58:43 -0700

    eval updated

--- eval.py
+++ eval.py
@@ -1,2 +1,5 @@
-expression=input("Enter your expression: ")
-print(f" entered expression : {eval(expression)}")
+value=input("Enter your expression: ")
+print(f" entered expression : {eval(value)}")
+user_value=eval(value)
+print(type(user_value))
+
