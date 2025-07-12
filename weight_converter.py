8267ddff4b65c1d9e6767e4ce62f0057cb78aad5
Veena Kuruba <javavk2022@gmail.com>
2025-07-12 12:04:52 -0700

    weight_converter.py

---
+++ weight_converter.py
@@ -0,0 +1,42 @@
+class weight:
+    """ A class to convert the weig"""
+    #ignore warnings
+    @staticmethod
+    def weight_input():
+        """Fet weight from user"""
+        try:
+            kgs_or_pounds=input("Enter wight in kgs or punds(kgs/pounds): ")
+            kgs_or_pounds=kgs_or_pounds.lower()
+            weight_user=input("Enter the weight : ")
+            return kgs_or_pounds, float(weight_user)
+        except Exception as e:
+            print(f"Invalid input {e}")
+            SystemExit()
+    @staticmethod
+    def converter(kgs_or_pounds, weight_user):
+        """Convert Kgs to pounds"""
+        if kgs_or_pounds=="kgs":
+            weight= weight_user * 2.20462
+            return weight
+        else:
+            weight=weight_user / 2.20462
+            return weight
+    @staticmethod
+    def print_weight(kgs_or_pounds,converter):
+        """Printing weight"""
+        if kgs_or_pounds=="kgs":
+            print(f"The user weight is : {converter} pounds")
+        else:
+            print(f"The user weight is : {converter} pounds")
+            
+    @staticmethod
+    def main():
+        kgs_or_pounds, weight_user=weight.weight_input()
+        converter=weight.converter(kgs_or_pounds,weight_user)
+        show_weight=weight.print_weight(kgs_or_pounds,converter)
+        return show_weight
+    
+weight.main()
+        
+        
+        
