b3b13bf040ccd5f12b4105554e5c9ee7b3f9d191
Veena Kuruba <javavk2022@gmail.com>
2025-07-11 23:34:19 -0700

    circumference_of_circle.py

--- circumference_of_circle.py
+++ circumference_of_circle.py
@@ -1,31 +1,38 @@
 import math
+class circum:
+    """circumetance of a circle"""
+    #ignore warnings
+    # This program calculates the circumference of a circle given its radius.
+    radius = 0
 
-"""circumetance of a circle"""
-#ignore warnings
-# This program calculates the circumference of a circle given its radius.
-radius = 0
-def get_radius():
-    """Calculate the circumetance of a circle given its radius."""
-    radius=input("Enter radius of the circle: ")
-    return float(radius)
-
-def calculate_circumference(radius):
-    circumetance_of_circle=2 * math.pi * radius
-    return circumetance_of_circle
-def print_results(radius,calculate):
-    print(f"the circumference of the circle with {radius} is {calculate}")
-    return radius,calculate
-# The main function to execute the program
-# pylint: disable=invalid-name  
-
-def main():
-    """Main function to execute the program."""
-    radius = get_radius()
-    calculate = calculate_circumference(radius)
-    value = print_results(radius, calculate)
-    return value
-calculate_circumference_circle=main()
-
-
+    @staticmethod
+    def get_radius():
+        try:
+            """Calculate the circumetance of a circle given its radius."""
+            radius=input("Enter radius of the circle: ")
+            return float(radius)
+        except Exception as e:
+            print("Invalid input, please enter a number for radius.")
+        SystemExit()
+    #ignore warnings
+    @staticmethod
+    def calculate_circumference(radius):
+            circumetance_of_circle=2 * math.pi * radius
+            return circumetance_of_circle
+    @staticmethod
+    def print_results(radius,calculate):
+        print(f"the circumference of the circle with {radius} is {calculate}")
+        return radius,calculate
+    # The main function to execute the program
+    # pylint: disable=invalid-name  
+    @staticmethod
+    def main():
+        """Main function to execute the program."""
+        radius = circum.get_radius()
+        calculate = circum.calculate_circumference(radius)
+        value = circum.print_results(radius, calculate)
+        return value
+    
+circum.main()
 
 
