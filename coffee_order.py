8a4bd1cefc3c4c0a74d57369979084ae20ee7331
Veena Kuruba <javavk2022@gmail.com>
2025-07-12 20:09:27 -0700

    Coffee

--- coffee_order.py
+++ coffee_order.py
@@ -3,7 +3,7 @@
 
 first_name=input("Enter your name: ")
 
-cofee=input("Enter your favourite coffee: Capuccino Espresso Latte ")
+cofee=input("What Coffee do you want to order?: Capuccino Espresso Latte Mocha Frappe: ")
 payment_option=input("Enter your payment option: Cash Credit Card Debit Card: ")
 amount=5.50
 
@@ -12,10 +12,10 @@ amount=5.50
 # Format the output with f string
 # pylint: disable=invalid-name
 
-print(f"Welcome {first_name} to our Restaurant!")
+print(f"Welcome {first_name} to our Cafe!")
 print(f"You have ordered {cofee} and payment option is: {payment_option} is this right?")
-if payment_option =="1":
-    print(f"Pay at cash counter {amount} dollars")
+if payment_option =="Cash":
+    print(f"Pay {amount} at cash counter dollars")
 else:
     print(f" Pay {amount} dollars in the next counter")
 you_are_done=True
