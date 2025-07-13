5216a0c96557b3048b5d356357a98d91691f1c82
Veena Kuruba <javavk2022@gmail.com>
2025-07-12 19:18:26 -0700

    airth

--- airth.py
+++ airth.py
@@ -1,5 +1,6 @@
 import math
 """This is a simple program to demonstrate the use of math module in Python."""
+# ignore warnings
 x=3.13
 y=4
 z=5.6
@@ -23,3 +24,15 @@ print(f"ceil/round up value of {x}is :{roundup_value}")
 #round down
 rounddown_value=math.floor(z)
 print(f"floor/round down value of {z} is :{rounddown_value}")
+a=input("Enter a: ")
+b=input("Enter b: ")
+
+max_number=a if a>b else b
+print(f"Maximum number is :{max_number}")
+min_number= a if a<b else b
+print(f"Minimum number is :{min_number}")
+age=int(input("Enter your age: "))
+eligible_to_login="An Adult" if age >18 else "Not an adult"
+print(f"User is {eligible_to_login}")
+
+
