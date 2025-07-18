17066c1c7894fb260fd6eb3439fc5e834f923f03
Veena Kuruba <javavk2022@gmail.com>
2025-07-18 00:14:20 -0700

    reve

--- reverse_string.py
+++ reverse_string.py
@@ -24,5 +24,28 @@ def credicard_fun():
     
     
 credicard_fun()
+def is_palindrome(x: int) -> bool:
+    """pallidrom number"""
+    if x < 0:
+        return False  # Negative numbers are never palindromes
+    
+    original = x
+    reversed_num = 0
+    
+    while x > 0:
+        digit = x % 10
+        reversed_num = reversed_num * 10 + digit
+        x //= 10
+    
+    return original == reversed_num
+x=int(input("enter value: "))
+# Examples
+if is_palindrome(x):
+    print(f"{x} is pallindrome")
+else:
+    print(f"{x} is not pallindrome")
+
+
+
     
     
